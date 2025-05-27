247005804af65a0ed231e6b1a6cc585b48998c08
Rishu Kashyap <rishusweta07@gmail.com>
2025-05-27 15:47:21 +0530

    initial setup done

---
+++ src/components/Header/header.jsx
@@ -0,0 +1,24 @@
+import './header.css';
+export default function Header() {
+    return(
+        
+            <div>
+                <ul>
+                    <li id="logo">
+                        <h3 id="first">Health</h3>
+                        <h3 id="second">care.</h3>
+                    </li>
+                    <li>
+                        <div>
+                            <ul>
+                                <li>search</li>
+                                <li><img src="./magnifying-glass.png" alt="not loaded" /></li>
+                            </ul>
+                        </div>
+                    </li>
+                </ul>
+            </div>
+        
+    )
+}
+
